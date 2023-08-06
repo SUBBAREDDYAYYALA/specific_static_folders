78c63a191bd5b176d1a4f19de8efc6fb5420fc59
SUBBAREDDYAYYALA <subbareddya2022@gmail.com>
2023-08-06 20:44:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kohli(request):
+    return render(request,'kohli.html')
+
+def indianteam(request):
+    return render(request,'indianteam.html')
+
+def rohit(request):
+    return render(request,'rohit.html')
+
+def kl(request):
+    return render(request,'kl.html')
+
+def bumrah(request):
+    return render(request,'bumrah.html')
+
+def hardik(request):
+    return render(request,'hardik.html')
+
+def siraj(request):
+    return render(request,'siraj.html')
+
+def shami(request):
+    return render(request,'shami.html')
+
+def chahal(request):
+    return render(request,'chahal.html')
+
+def gill(request):
+    return render(request,'gill.html')
+
+def iyer(request):
+    return render(request,'iyer.html')
+
+def ishan(request):
+    return render(request,'ishan.html')
+
+def surya(request):
+    return render(request,'surya.html')
+
+def sanju(request):
+    return render(request,'sanju.html')
+
+def ruturaj(request):
+    return render(request,'ruturaj.html')
+
+def rahane(request):
+    return render(request,'rahane.html')
